618732360b8e35e0efe2699ca8629ee5e51db4af
UchihaMdara1 <2898831413@qq.com>
2019-08-27 21:39:52 +0800

    日常功课

--- homework-8-26/homework-8-26/homework-8-26.c
+++ homework-8-26/homework-8-26/homework-8-26.c
@@ -89,8 +89,92 @@
 //	printf("%d\n",ret);
 //	return 0;
 //}
-int main()
-{
-
-	return 0;
-}
+#include <string.h>
+#include <assert.h>
+//char * my_strcpy(char *dest, const char *src)
+//{
+//	assert(dest && src);
+//	char *ret = dest;
+//	while (*dest++ = *src++)
+//	{
+//		;
+//	}
+//	return ret;
+//}
+//int main()
+//{
+//	const char *p = "yihuayishijie";
+//	char arr[30] = {0};
+//	my_strcpy (arr, p);
+//	printf("%s\n",arr);
+//	return 0;
+//}
+//char * my_strcat(char *dest, const char *src)
+//{
+//	assert(dest && src);
+//	char *ret = dest;
+//	while (*dest != '\0')
+//	{
+//		dest++;
+//	}
+//	while (*dest++ = *src++)
+//	{
+//		
+//	}
+//	return ret;
+//}
+//int main()
+//{
+//	char arr[20] = "hello ";
+//	char *p = "world";
+//	my_strcat(arr,p);
+//	printf("%s\n",arr);
+//	return 0;
+//}
+//void * my_memcpy(void *dest,void *src, size_t count)
+//{
+//	assert(dest && src);
+//    void *ret = src;
+//	while (count--)
+//	{
+//		*(char *)dest = *(char *)src;
+//		dest = (char *)dest + 1;
+//		src = (char *)src + 1;
+//	}
+//	return ret;
+//}
+//int main()
+//{
+//	int arr1[] = {1,2,3,4,5,6,6,7};
+//	int arr2[20] = { 0 };
+//	my_memcpy(arr2, arr1, 28);
+//	return 0;
+//}
+//void * my_memmove(void *left, void * right, size_t count)
+//{
+//	assert(left && right);
+//	void * ret = left;
+//		if (left > right)
+//		{
+//			while (count--)
+//			{
+//				*((char *)left + count) = *((char *)right + count);
+//			}
+//		}
+//		else
+//		{
+//			while (count--)
+//			{
+//				*(char *)left = *(char *)right;
+//				left = (char *)left + 1;
+//				right = (char *)right + 1;
+//			}
+//		}
+//		return ret;
+//}
+//int main()
+//{
+//	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//	my_memmove(arr +2,arr, 16);
+//	return 0;
+//}
