1df7e0097fca91306f04deba2f3364671b8ce218
UchihaMdara1 <2898831413@qq.com>
2019-09-17 00:16:59 +0800

    练习

---
+++ homework-9-16/homework-9-16/test.c
@@ -0,0 +1,63 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	char a = 1;
+//	int b = 2;
+//	float c = 3;
+//	double d = 4;
+//	a * b + d - c;
+//	return 0;
+//}
+//int main()
+//{
+//	int i,j,a = 0;
+//	for (i = 0; i < 2; i++)
+//	{
+//		for (j = 0; j < 4; j++)
+//		{
+//			if (j % 2)
+//				break;
+//			a++;
+//		}
+//		a++;
+//	}
+//	printf("%d\n",a);
+//	return 0;
+//}
+//int main()
+//{
+//	int a,i = 0;
+//	int j = 3;
+//	int arr[4][4] = {1,2,8,9,
+//	                 2,4,9,12,
+//	                 4,7,10,13,
+//	                 6,8,11,15};
+//	scanf("%d",&a);
+//	while (i < 4 && j >= 0)
+//	{
+//		if (arr[i][j] > a)
+//		{
+//			j--;
+//		}
+//		else if (arr[i][j] < a)
+//		{
+//			i++;
+//		}
+//		else
+//		{
+//			printf("сп\n");
+//			return 0;
+//		}
+//	}
+//	printf("ц╩сп\n");
+//	return 0;
+//}
+//#define INT_PTR int*
+//typedef  int* int_ptr;
+//int main()
+//{
+//	INT_PTR a, b;
+//	int_ptr c, d;
+//	return 0;
+//}
